17654467d7cd2420c964d1191dd09d5149972366
SleepyJacket <sean_timothy_co@dlsu.edu.ph>
2022-05-04 17:25:55 +0800

    login page and initial registration page

--- app/src/main/java/ph/edu/dlsu/mobdeve/co/sean/evangelista/jason/finalproject/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/co/sean/evangelista/jason/finalproject/MainActivity.kt
@@ -1,7 +1,9 @@
 package ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        binding.btnLogin.setOnClickListener { view: View? ->
+//            var goToHome = Intent(this, HomeActivity::class.java)
+//            startActivity(goToHome)
+//        }
+
+        binding.btnToRegister.setOnClickListener { view: View? ->
+            var goToRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(goToRegister)
+        }
+
     }
 }
---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/co/sean/evangelista/jason/finalproject/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
