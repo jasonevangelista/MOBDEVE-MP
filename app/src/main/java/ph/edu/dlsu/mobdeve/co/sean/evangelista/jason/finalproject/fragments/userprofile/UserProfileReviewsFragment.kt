674d847b10ee9793da92fea081d30c83f6333a72
jasonevangelista <jase_evangelista@dlsu.edu.ph>
2022-05-19 11:24:01 +0800

    connected reviewArrayList to Player Reviews Page

--- app/src/main/java/ph/edu/dlsu/mobdeve/co/sean/evangelista/jason/finalproject/fragments/userprofile/UserProfileReviewsFragment.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/co/sean/evangelista/jason/finalproject/fragments/userprofile/UserProfileReviewsFragment.kt
@@ -6,16 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.AddReviewActivity
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.R
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.UserProfileActivity
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.adapter.PlayerAdapter
+import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.adapter.ReviewAdapter
+import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.dao.ReviewsDAO
+import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.dao.ReviewsDAOArrayImpl
 import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.databinding.FragmentUserProfileReviewsBinding
+import ph.edu.dlsu.mobdeve.co.sean.evangelista.jason.finalproject.model.Review
+import java.util.*
 
 class UserProfileReviewsFragment : Fragment(R.layout.fragment_user_profile_reviews) {
 
     private var _binding : FragmentUserProfileReviewsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var reviewArrayList: ArrayList<Review>
+    private lateinit var reviewAdapter: ReviewAdapter
+    private lateinit var viewManager : LinearLayoutManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +42,15 @@ class UserProfileReviewsFragment : Fragment(R.layout.fragment_user_profile_revie
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
 
+        viewManager = LinearLayoutManager(activity)
+        reviewAdapter = ReviewAdapter(requireContext(), reviewArrayList)
+
+        binding.rvPlayerReviews.apply {
+            layoutManager = viewManager
+            adapter = reviewAdapter
+        }
 
         binding.btnAddReview.setOnClickListener {
             val goToAddReview = Intent(activity, AddReviewActivity::class.java)
@@ -44,6 +63,28 @@ class UserProfileReviewsFragment : Fragment(R.layout.fragment_user_profile_revie
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun init(){
+        var dao: ReviewsDAO = ReviewsDAOArrayImpl()
+
+        var review1 = Review()
+        review1.sender = "player2"
+        review1.receiver = "player1"
+        review1.content = "Great teammate!!!"
+        review1.rating = 5.0F
+        review1.date = Date()
+        dao.addReview(review1)
+
+        dao.addReview(review1)
+        dao.addReview(review1)
+        dao.addReview(review1)
+        dao.addReview(review1)
+        dao.addReview(review1)
+        dao.addReview(review1)
+
+        reviewArrayList = dao.getReviews()
+
     }
 
 }
